831fdc24bad43b563e2c1656d51c3537e4b13429
Fake0Fully <jackguonfls@outlook.com>
2016-09-26 00:18:48 -0400

    Fixed timezone

--- bash_script.sh
+++ bash_script.sh
@@ -4,7 +4,7 @@ sudo apt-key adv -keyserver keyserver.ubuntu.com -recv-keys E084DAB9
 sudo add-apt-repository 'deb http://star-www.st-andrews.ac.uk/cran/bin/linux/ubuntu trusty/'
 sudo apt-get -y update
 sudo apt-get install -y --force-yes r-base-core
-sudo su -\-c "R -e \"install.packages( c('Rcpp' ,'bitops' ,'tools' ,'packrat' ,'digest' ,'jsonlite' ,'gtable' ,'lattice' ,'yaml' ,'proto' ,'htmlwidgets' ,'grid' ,'R6' ,'ramazon' ,'RJSONIO' ,'magrittr' ,'scales' ,'htmltools' ,'MASS' ,'mime' ,'xtable' ,'colorspace' ,'httpuv' ,'stringi' ,'RCurl' ,'munsell' ,'stats' ,'graphics' ,'grDevices' ,'utils' ,'datasets' ,'methods' ,'base' ) , repos = 'http://cran.rstudio.com/', dep = TRUE)\""
+sudo su -\-c "R -e \"install.packages( c('Rcpp' ,'bitops' ,'tools' ,'digest' ,'packrat' ,'gtable' ,'lattice' ,'yaml' ,'proto' ,'htmlwidgets' ,'grid' ,'R6' ,'ramazon' ,'RJSONIO' ,'magrittr' ,'scales' ,'htmltools' ,'MASS' ,'mime' ,'xtable' ,'colorspace' ,'httpuv' ,'stringi' ,'RCurl' ,'munsell' ,'stats' ,'graphics' ,'grDevices' ,'utils' ,'datasets' ,'methods' ,'base' ) , repos = 'http://cran.rstudio.com/', dep = TRUE)\""
 echo 'R installed'
 sudo apt-get install -y gdebi-core
 wget http://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.3.0.403-amd64.deb
--- test.R
+++ test.R
@@ -1,17 +1,18 @@
 dat <- read.csv('data/merged.csv',stringsAsFactors = FALSE, header=T)
 dat[,"entry_time"] <- as.POSIXct(dat[,"entry_time"], format="%Y-%m-%d %H:%M:%S", tz="Asia/Singapore")
 dat[,"exit_time"] <- as.POSIXct(dat[,"exit_time"], format="%Y-%m-%d %H:%M:%S", tz="Asia/Singapore")
-input_date <- c('2015-06-24','2015-06-25','2015-06-26')
+input_date <- c('2016-01-21','2016-01-22')
 input_time <- c('6:00:00','18:00:00')
+time_filter <- c(6,18)
 input_interval <- 50
-parking <- NULL
+park_location <- NULL
+mall_filter <- 'Mall 2'
 
+start <- Sys.time()
 temp <- dat
-temp <- subset(temp, temp$date %in% input_date)
-temp <- subset(temp, hour(temp$entry_time)>=6&hour(temp$entry_time)<=18)
 temp <- temp[!is.na(temp[,"entry_time"]) & !is.na(temp[,"exit_time"]),c("park_location", "entry_time", "exit_time", "dtime")]
-if (!is.null(parking)){
-  temp <- subset(temp,temp$park_location %in% parking)
+if (!is.null(park_location)){
+  temp <- subset(temp,temp$park_location %in% park_location)
 }
 entries <- data.frame(cbind(as.character(temp[,"entry_time"]), rep(1,length(temp[,"entry_time"]))), stringsAsFactors=F)
 exits <- data.frame(cbind(as.character(temp[,"exit_time"]), rep(-1,length(temp[,"exit_time"]))), stringsAsFactors=F)
@@ -25,7 +26,6 @@ while (max(table(no_agents[,"time"])) >1) {
   no_agents <- no_agents[order(no_agents[,"time"] ),]
 }
 
-
 no_agents[,2] <- as.numeric(no_agents[,2])
 no_agents[,3] <- NA
 no_agents[1,3] <- no_agents[1,2]
@@ -33,33 +33,42 @@ for (j in 2:nrow(no_agents)) no_agents[j,3] <- no_agents[j-1,3]+no_agents[j,2]
 
 no_agents <- no_agents[,c(1,3)]
 names(no_agents) <- c('time','number')
-no_agents[,"time"] <- as.POSIXct(no_agents[,"time"], format="%Y-%m-%d %H:%M:%S", tz="Asia/Singapore")
 
 
-arrivals_table <- data.frame(rep(NA, 
-                                 length(seq(from = as.POSIXct(paste(input_date[1], input_time[1]), format="%Y-%m-%d %H:%M:%S", tz="Asia/Singapore"), 
-                                            to = as.POSIXct(paste(input_date[1], input_time[2]), format="%Y-%m-%d %H:%M:%S", tz="Asia/Singapore"), 
-                                            by=1))))
+system_table <- data.frame(rep(NA, 
+                               length(seq(from = as.POSIXct(paste(input_date[1], input_time[1]), format="%Y-%m-%d %H:%M:%S", tz="Asia/Singapore"), 
+                                          to = as.POSIXct(paste(input_date[1], input_time[2]), format="%Y-%m-%d %H:%M:%S", tz="Asia/Singapore"), 
+                                          by=1))))
 
-names(arrivals_table) <- 'time'
+names(system_table) <- 'time'
 
 for (i in 1:length(input_date)) {
-  arrivals_table[,1] <- as.POSIXct(seq(from = as.POSIXct(paste(input_date[i], input_time[1]), format="%Y-%m-%d %H:%M:%S", tz="Asia/Singapore"), 
-                                       to = as.POSIXct(paste(input_date[i], input_time[2]), format="%Y-%m-%d %H:%M:%S", tz="Asia/Singapore"), 
-                                       by=1), format="%Y-%m-%d %H:%M:%S", tz="Asia/Singapore")
-  arrivals_table <- merge(arrivals_table,no_agents,by='time',all.x=TRUE)
-  arrivals_table[1,i+1] <- 0
-  arrivals_table[i+1] <- na.locf(arrivals_table[i+1])
+  system_table[,1] <- seq(from = as.POSIXct(paste(input_date[i], input_time[1]), format="%Y-%m-%d %H:%M:%S", tz="Asia/Singapore"), 
+                                     to = as.POSIXct(paste(input_date[i], input_time[2]), format="%Y-%m-%d %H:%M:%S", tz="Asia/Singapore"), 
+                                     by=1)
+  system_table <- merge(system_table,no_agents,by='time',all.x=TRUE)
+  system_table[1,i+1] <- 0
+  system_table[i+1] <- na.locf(system_table[i+1])
 }
-rownames(arrivals_table) <- as.POSIXct(arrivals_table[,1],format="%Y-%m-%d %H:%M:%S", tz="Asia/Singapore")
-names(arrivals_table) <- c("aaa", format(as.Date(input_date, tz="Asia/Singapore"), format="%B %d %Y"))
-arrivals_table <- as.xts(arrivals_table)
-arrivals_table <- arrivals_table[,-1]
+names(system_table) <- c("aaa", format(as.Date(input_date, tz="Asia/Singapore"), format="%B %d %Y"))
+mytime <- system_table[,1]
+myvalue <- system_table[,c(2,3)]
 
-title_plot <- paste("Number of arrivals per", "minute intervals") 
-dygraph(arrivals_table, main=title_plot) %>% 
-  dyHighlight(highlightCircleSize = 5, highlightSeriesBackgroundAlpha = 0.2, hideOnMouseOut = FALSE) %>%
+system_table <- xts(myvalue,order.by=mytime,tz='Asia/Singapore')
+
+stepplot <- dygraph(system_table, main="Total number of goods vehicles in the system") %>% 
+  dyHighlight(highlightCircleSize = 3, highlightSeriesBackgroundAlpha = 0.2, hideOnMouseOut = FALSE) %>%
   dyLegend(labelsSeparateLines=T, width=170) %>%
-  dyAxis("y", label="No. vehicles observed") %>%
-  dyAxis("x", label="Start of interval") %>% 
-  dyOptions(useDataTimezone = F)
+  dyAxis("y", label="No. goods vehicles") %>%
+  dyAxis("x", label="Time") %>% 
+  dyOptions(useDataTimezone = T)
+if (mall_filter=='Mall 1'){
+  stepplot <- dyLimit(stepplot, 6, color = 'red')
+}else if (mall_filter=='Mall 2'){
+  stepplot <- dyLimit(stepplot, 18, color = 'green')
+}else{
+  stepplot <- dyLimit(stepplot, 6, color = 'red')
+  stepplot <- dyLimit(stepplot, 18, color = 'green')
+}
+stepplot
+Sys.time()-start
